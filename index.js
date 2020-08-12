7114a7070eb20e398a0da5fbd1c1ade65818992f
Elham Fabe <elhamfabe@gmail.com>
2020-08-11 21:03:55 -0400

    added content to generateMarkdown & index

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 //  fs is a Node standard library package and writing files
 const inquire = require("require");
 const fs = require ("fs");
+const utils = require ("utils");
+const generateMarkdown = require ("./utils/generateMarkdown");
+
+
+
 //  generate READMe
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,18 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return
+   `# ${data.title}
+  Contents of This File
+  ---
+  #Introduction
 
+  ## Description
+
+  ### Requirements
+
+  ### Installation
+  
+  #### License 
 `;
 }
 
