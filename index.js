f39fc9b63d11628306ef9e6bb88143e4122555eb
Elham Fabe <elhamfabe@gmail.com>
2020-08-12 13:20:53 -0400

    prompts

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 //  fs is a Node standard library package and writing files
 const inquire = require("require");
-const fs = require ("fs");
-const utils = require ("utils");
-const generateMarkdown = require ("./utils/generateMarkdown");
-
+const fs = require("fs");
+const utils = require("utils");
+const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require("inquirer");
 
 
 //  generate READMe
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the name of your application:",
+            name: "title"
+        },
+        {
+            type: "input",
+             message: "Give you applicaiton a description",
+             name: "description"
+
+        },
+        {
+            type: "checkbox",
+            message: "Select a license.",
+            choices: [
+                "Apache",
+                "MIT",
+                "ISC",
+                "None"
+            ],
+
+        },
+    ])
+}
